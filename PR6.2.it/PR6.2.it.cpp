778b8e6f0f38db2c9a7712bb0373d320f69f37e3
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-02 10:01:33 +0200

    pr6.2.it

---
+++ PR6.2.it/PR6.2.it.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+void printArray(const int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+}
+int maxEvenIndex(int a[], int n) {
+    int index = -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0 && a[i] > a[index]) {
+            index = i;
+        }
+    }
+    return index;
+}
+int main() {
+    int n = 8;
+    int a[8] = { 1,6,7,4,3,9,2,10};
+    printArray(a, n);
+    int  index = maxEvenIndex(a, n);
+    cout << "maxEvenIndex =" << index << endl;
+    return 0;
+}
---
+++ UnitTest6.2.it/UnitTest6.2.it.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR6.2.it/PR6.2.it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62it
+{
+	TEST_CLASS(UnitTest62it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 5;
+			int a[5] = { 2,5,4,3,6 };
+			int index= maxEvenIndex(a, 5);
+			Assert::AreEqual(4, index);
+		}
+	};
+}
